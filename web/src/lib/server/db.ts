cd373d022fada424732791a4d362264a0524f6d3
Madhav Kandukuri <madhav165@gmail.com>
2026-05-10 15:17:40 +0530

    fix: return DATE columns as strings to avoid timezone skew in UI

--- web/src/lib/server/db.ts
+++ web/src/lib/server/db.ts
@@ -1,6 +1,9 @@
 import pg from 'pg'
 import { DATABASE_URL } from '$env/static/private'
 
+// Return DATE columns as plain YYYY-MM-DD strings to avoid timezone skew
+pg.types.setTypeParser(pg.types.builtins.DATE, (val) => val)
+
 const pool = new pg.Pool({ connectionString: DATABASE_URL })
 
 export const db = {
